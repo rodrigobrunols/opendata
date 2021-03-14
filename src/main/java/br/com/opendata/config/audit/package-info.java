ad9f80ff60ffeeda9b0458fb13493ea1f579eed0
rodrigo.leoncio <rodrigorunols@gmail.com>
2021-03-14 01:30:51 -0300

    Initial application generated by JHipster-6.1.2

---
+++ src/main/java/br/com/opendata/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package br.com.opendata.config.audit;
---
+++ src/main/java/br/com/opendata/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package br.com.opendata.repository;
+
+import br.com.opendata.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
